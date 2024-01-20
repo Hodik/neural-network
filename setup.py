3fbc4c7f05663197178c40b2440490e602cfec27
denis.khodishchenko <denys@hennessey.com>
2024-01-20 20:14:34 +0200

    package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="neural_network",
+    name="nn",
     version="0.1",
     packages=find_packages(),
     description="A simple example package",
